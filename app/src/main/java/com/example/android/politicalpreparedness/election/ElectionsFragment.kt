9ad98dfdc91635329e24f6032528b4382d42bfe1
azzumwaqar <azzumwaqar@gmail.com>
2022-06-19 15:04:46 +0100

    App Compiling - Navigation Working

---
+++ app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsFragment.kt
@@ -0,0 +1,43 @@
+package com.example.android.politicalpreparedness.election
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.android.politicalpreparedness.R
+import com.example.android.politicalpreparedness.databinding.FragmentElectionBinding
+
+class ElectionsFragment: Fragment() {
+
+    //TODO: Declare ViewModel
+     var _binding : FragmentElectionBinding? = null
+    val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_election,container,false)
+
+        //TODO: Add ViewModel values and create ViewModel
+
+        //TODO: Add binding values
+
+        //TODO: Link elections to voter info
+
+        //TODO: Initiate recycler adapters
+
+        //TODO: Populate recycler adapters
+        return binding.root
+    }
+
+    //TODO: Refresh adapters when fragment loads
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
