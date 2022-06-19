80995f6f0aa584dc8d9e153a0d50a5c4b61498ad
azzumwaqar <azzumwaqar@gmail.com>
2022-06-19 15:26:09 +0100

    clean

--- app/src/main/java/com/example/android/politicalpreparedness/launch/LaunchFragment.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/launch/LaunchFragment.kt
@@ -8,29 +8,34 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.android.politicalpreparedness.R
 import com.example.android.politicalpreparedness.databinding.FragmentLaunchBinding
+
 //import com.example.android.politicalpreparedness.election.adapter.ElectionListAdapter
 //import com.example.android.politicalpreparedness.election.adapter.ElectionListener
 
 class LaunchFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater,
-                              container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         val binding = FragmentLaunchBinding.inflate(inflater)
         binding.lifecycleOwner = this
 
-        binding.findRepresentativeButton .setOnClickListener { navToRepresentatives() }
+        binding.findRepresentativeButton.setOnClickListener { navToRepresentatives() }
         binding.upComingElectionsButton.setOnClickListener { navToElections() }
 
         return binding.root
     }
 
     private fun navToElections() {
-        this.findNavController().navigate(LaunchFragmentDirections.actionLaunchFragmentToElectionsFragment())
+        this.findNavController()
+            .navigate(LaunchFragmentDirections.actionLaunchFragmentToElectionsFragment())
     }
 
     private fun navToRepresentatives() {
-        this.findNavController().navigate(LaunchFragmentDirections.actionLaunchFragmentToRepresentativeFragment())
+        this.findNavController()
+            .navigate(LaunchFragmentDirections.actionLaunchFragmentToRepresentativeFragment())
     }
 
 }
