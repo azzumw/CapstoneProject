4abb4062c6541db376dbeaa4fbb6cc1f3738dcdc
azzumwaqar <azzumwaqar@gmail.com>
2022-06-24 14:58:26 +0100

    Dao compiling without suspend

--- app/src/main/java/com/example/android/politicalpreparedness/database/ElectionDao.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/database/ElectionDao.kt
@@ -7,26 +7,29 @@ import com.example.android.politicalpreparedness.network.models.Election
 @Dao
 interface ElectionDao {
 
+    // ISSUE: when adding suspend keyword!
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertAllElections(elections:List<Election>)
+    fun insertAllElections(elections:List<Election>)
 
-    // Add insert query
+//    // Add insert query
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun saveElection(election: Election)
+    fun saveElection(election: Election)
 
-    // Add select all election query
+//    // Add select all election query
     @Query("select * from election_table")
     fun getAllElections():LiveData<List<Election>>
+//
+//    // Add select single election query
 
-    // Add select single election query
-    fun getAnElection():LiveData<Election>
-
-    // Add delete query
+    @Query("select * from election_table where id = :id")
+    fun getAnElection(id:Int):LiveData<Election>
+//
+//    // Add delete query
     @Delete
     fun deleteElection(election: Election)
 
-    // Add clear query
+//    // Add clear query
     @Query("delete from election_table")
     fun clear()
 }
