bd532b6c6f45071329442fb1b85e79b8b3920391
azzumwaqar <azzumwaqar@gmail.com>
2023-01-29 20:08:11 +0000

    tests - Ready for next testing

--- app/src/androidTest/java/MainActivityTests.kt
+++ app/src/androidTest/java/MainActivityTests.kt
@@ -1,18 +1,13 @@
 import androidx.test.core.app.ActivityScenario.launch
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.example.android.politicalpreparedness.MainActivity
 import com.example.android.politicalpreparedness.R
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 
 class MainActivityTests {
-
 
 
     @Before
@@ -21,8 +16,9 @@ class MainActivityTests {
 
     @Test
     fun launchActivity() {
-       launch(MainActivity::class.java)
+        launch(MainActivity::class.java)
 
-//        onView(withId(R.id.upComingElectionsButton))
-//            .perform(click())
-    }}
+        onView(withId(R.id.upComingElectionsButton))
+            .perform(click())
+    }
+}
